be17aa937d85db1353814036dea0fc241c422963
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-12-22 23:09:50 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '../Slider/Slider';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <Slider></Slider>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+
+import img1 from '../../../assets/slider/laptop.webp'
+import img2 from '../../../assets/slider/smartwatch.webp'
+import img3 from '../../../assets/slider/phone.webp'
+import img4 from '../../../assets/slider/headphone.webp'
+import img5 from '../../../assets/slider/camera.webp'
+import img6 from '../../../assets/slider/monitor.webp'
+
+
+const Slider = () => {
+    return (
+        <div>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                slidesPerGroup={3}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={img6} alt="" /></SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
