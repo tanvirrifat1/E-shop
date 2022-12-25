88603eedd1702af319f2271110252bf35e1a9ca0
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-12-26 00:43:35 +0600

    added transition

--- src/Pages/Home/Collection/Collection.js
+++ src/Pages/Home/Collection/Collection.js
@@ -5,7 +5,7 @@ const Collection = ({ cloth }) => {
     const { name, description, img } = cloth
     return (
         <div>
-            <div className="card card-compact shadow-xl">
+            <div className="card card-compact shadow-xl card-details">
                 <figure><img className='size' src={img} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
