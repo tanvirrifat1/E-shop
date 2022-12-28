1e5e05bfa82b00918cd19ad3777ab634f24f4ddd
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-12-28 22:36:28 +0600

    added login structure

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import image from '../../assets/login/sign-concept-illustration_114360-125.webp'
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login</h1>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl'>Login</h2>
+            </div>
+            <div >
+
+            </div>
+
         </div>
     );
 };
